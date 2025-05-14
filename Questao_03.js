8cbc84395f1f84b656679a23db60625090a79820
Deborange <101590106+DeboraBee@users.noreply.github.com>
2025-05-14 20:04:32 -0300

    Create Questao_03.js

---
+++ Questao_03.js
@@ -0,0 +1,25 @@
+// Aluno 1
+let nome1 = "Debora Laranjeira";
+let nota1_1 = 7;
+let nota1_2 = 8.5;
+let peso1 = nota1_1 * 0.6;
+let peso2 = nota1_2 * 0.4;
+let notaFinal1 = peso1 + peso2;
+alert("Aluno: " + nome1 + "\nNota 1: " + nota1_1 + "\nNota 2: " + nota1_2);
+alert("Nota final de " + nome1 + ": " + notaFinal1);
+
+// Aluno 2
+let nome2 = "Miguel Oliveira";
+let nota2_1 = 9;
+let nota2_2 = 7.5;
+let notaFinal2 = nota2_1 * 0.6 + nota2_2 * 0.4;
+alert("Aluno: " + nome2 + "\nNota 1: " + nota2_1 + "\nNota 2: " + nota2_2);
+alert("Nota final de " + nome2 + ": " + notaFinal2);
+
+// Aluno 3
+let nome3 = "Giovanna Menezes";
+let nota3_1 = 6;
+let nota3_2 = 8;
+let notaFinal3 = nota3_1 * 0.6 + nota3_2 * 0.4;
+alert("Aluno: " + nome3 + "\nNota 1: " + nota3_1 + "\nNota 2: " + nota3_2);
+alert("Nota final de " + nome3 + ": " + notaFinal3);
